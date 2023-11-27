a1108e148cee89473d4902147986b95da5bc93f1
vsmanigithub <151604798+vsmanigithub@users.noreply.github.com>
2023-11-27 22:20:41 +0530

    Update HelloWorld.java
    few lines

--- HelloWorld.java
+++ HelloWorld.java
@@ -35,9 +35,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello, World");
-    System.out.println("this is madhu");
-    System.out.println("Hello, Banglore");
-    System.out.println("Hello, canada");
-    ;
   }
 }
